81d0bd84c57cb38e6d2b024615f0cf43be25b611
Josh Wood <joshuawood2894@gmail.com>
2021-03-13 16:02:51 -0600

    added normalize.css

--- src/main/app/server_components/middleware.clj
+++ src/main/app/server_components/middleware.clj
@@ -39,6 +39,7 @@
       [:title "Application"]
       [:meta {:charset "utf-8"}]
       [:meta {:name "viewport" :content "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"}]
+      [:link {:ref "stylesheet" :href "css/normalize.css"}]
       [:link {:href "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
               :rel  "stylesheet"}]
       [:link {:rel "shortcut icon" :href "data:image/x-icon;," :type "image/x-icon"}]
